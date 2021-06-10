64d8849e8c5a5ef334f14c2dace39d370394b6fe
Naveen132895 <veeranaveen.sethuraman@faceacademy.in>
2021-06-10 13:28:59 +0530

    Final Commit

---
+++ src/components/tourData.js
@@ -0,0 +1,32 @@
+
+export const tourData = [
+    {
+      id: 1,
+      city: "Agra",
+      img: "./img/agra.jpg",
+      name: "Agra Taj Mahal tour",
+      info: "My India My Proud.मेरा भारत महान"
+    },
+    {
+      id: 2,
+      city: "Jaipur",
+      img: "./img/jaipur.jpg",
+      name: "Jaipur Amber Fort tour",
+      info:"My India My Proud.मेरा भारत महान"
+    },
+    {
+      id: 3,
+      city: "Mumbai",
+      img: "./img/mumbai.jpg",
+      name:"Mumbai India Gate tour",
+      info:"My India My Proud.मेरा भारत महान"
+    },
+    {
+      id: 4,
+      city: "Munnar",
+      img: "./img/munnar.jpg",
+      name: "Munnar Kundala Lake tour",
+      info: "My India My Proud.मेरा भारत महान"
+    }
+  ];
+  
